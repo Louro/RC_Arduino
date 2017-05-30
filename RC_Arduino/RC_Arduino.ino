c7bf583aa6eb11896e79e992fbd28d21d07d2928
Paulo Louro <pjc@au2mate.dk>
2017-05-30 18:01:42 +0200

    Add project files.

---
+++ RC_Arduino/RC_Arduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		RC_Arduino.ino
+ Created:	5/30/2017 6:01:40 PM
+ Author:	pjc
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
